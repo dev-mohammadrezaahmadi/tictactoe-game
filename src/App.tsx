d098c030476a2c31ebc512e05a36a50e9a58ab80
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-08-19 23:41:59 +0430

    add functionality to board

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Game: React.FC = () => {
 	return (
 		<div className="flex flex-col justify-center items-center min-h-screen">
@@ -8,24 +10,86 @@ const Game: React.FC = () => {
 };
 
 const Board: React.FC = () => {
-	const renderCell = () => (
-		<button className="flex items-center justify-center bg-white border border-gray-500 rounded-sm focus:bg-red-50 focus:outline-none font-bold h-20 w-20 text-3xl md:h-28 md:w-28 md:text-5xl lg:text-8xl lg:h-40 lg:w-40">
-			X
+	const [cells, setCells] = useState<BoardType>(Array(9).fill(null));
+
+	const nextValue = _calculateNextValue(cells);
+	const winner = _calculateWinner(cells);
+
+	const selectCell = (cellIndex: number) => {
+		// check if there's already a winner or there's already a value in the selected cell
+		if (winner || cells[cellIndex]) return;
+
+		const cellsCopy = [...cells];
+		cellsCopy[cellIndex] = nextValue;
+		setCells(cellsCopy);
+	};
+
+	const reset = () => {
+		setCells(Array(9).fill(null));
+	};
+
+	const renderCell = (i: number) => (
+		<button
+			onClick={() => selectCell(i)}
+			className="flex items-center justify-center bg-white border border-gray-500 rounded-sm focus:bg-red-50 focus:outline-none font-bold h-20 w-20 text-3xl md:h-28 md:w-28 md:text-5xl lg:text-8xl lg:h-40 lg:w-40"
+		>
+			{cells[i]}
 		</button>
 	);
+
 	return (
-		<div className="mx-auto grid grid-cols-3">
-			{renderCell()}
-			{renderCell()}
-			{renderCell()}
-			{renderCell()}
-			{renderCell()}
-			{renderCell()}
-			{renderCell()}
-			{renderCell()}
-			{renderCell()}
-		</div>
+		<>
+			<div className="mx-auto grid grid-cols-3">
+				{renderCell(0)}
+				{renderCell(1)}
+				{renderCell(2)}
+				{renderCell(3)}
+				{renderCell(4)}
+				{renderCell(5)}
+				{renderCell(6)}
+				{renderCell(7)}
+				{renderCell(8)}
+			</div>
+			<button
+				className="mt-3 p-2 uppercase bg-black text-white font-bold rounded-md"
+				onClick={reset}
+			>
+				reset
+			</button>
+		</>
 	);
 };
 
 export default Game;
+
+// utils
+
+type CellValueType = "X" | "O" | null;
+type BoardType = CellValueType[];
+
+const _calculateNextValue = (cells: BoardType): CellValueType => {
+	return cells.filter(Boolean).length % 2 === 0 ? "X" : "O";
+};
+
+const _calculateWinner = (cells: BoardType): CellValueType => {
+	// states of winnig
+	const lines: number[][] = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+			return cells[a];
+		}
+	}
+
+	return null;
+};
