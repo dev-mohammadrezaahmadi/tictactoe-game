37d9a6edecbfc93d8159c867c8ac539b0439d2a6
dev-mohammadrezaahmadi <dev.mohammadrezaahmadi@gmail.com>
2021-08-19 22:40:51 +0430

    initial style of the game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const Game: React.FC = () => {
+	return (
+		<div className="flex flex-col justify-center items-center min-h-screen">
+			<h1 className="text-5xl font-bold mb-10">Tic Tac Toe</h1>
+			<Board />
+		</div>
+	);
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Board: React.FC = () => {
+	const renderCell = () => (
+		<button className="flex items-center justify-center bg-white border border-gray-500 rounded-sm focus:bg-red-50 focus:outline-none font-bold h-20 w-20 text-3xl md:h-28 md:w-28 md:text-5xl lg:text-8xl lg:h-40 lg:w-40">
+			X
+		</button>
+	);
+	return (
+		<div className="mx-auto grid grid-cols-3">
+			{renderCell()}
+			{renderCell()}
+			{renderCell()}
+			{renderCell()}
+			{renderCell()}
+			{renderCell()}
+			{renderCell()}
+			{renderCell()}
+			{renderCell()}
+		</div>
+	);
+};
 
-export default App;
+export default Game;
